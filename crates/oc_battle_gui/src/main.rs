203e4a2a878d1aad7f7ddf309917a29919f364e6
Bastien Sevajol <contact@bux.fr>
2026-02-19 22:48:43 +0100

    feat: debug as feature

--- crates/oc_battle_gui/src/main.rs
+++ crates/oc_battle_gui/src/main.rs
@@ -1,11 +1,7 @@
 use std::net::SocketAddr;
 
+use bevy::prelude::*;
 use bevy::sprite_render::Wireframe2dPlugin;
-use bevy::{
-    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin, FrameTimeGraphConfig},
-    prelude::*,
-    text::FontSmoothing,
-};
 use clap::Parser;
 
 use crate::{
@@ -13,6 +9,11 @@ use crate::{
     network::NetworkPlugin, states::AppState,
 };
 
+#[cfg(feature = "debug")]
+use debug::DebugPlugin;
+
+#[cfg(feature = "debug")]
+mod debug;
 mod entity;
 mod error;
 mod home;
@@ -33,35 +34,9 @@ pub struct Args_ {
 pub struct Args(pub Args_);
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    App::new()
-        .add_plugins((
-            DefaultPlugins,
-            FpsOverlayPlugin {
-                config: FpsOverlayConfig {
-                    text_config: TextFont {
-                        // Here we define size of our overlay
-                        font_size: 42.0,
-                        // If we want, we can use a custom font
-                        font: default(),
-                        // We could also disable font smoothing,
-                        font_smoothing: FontSmoothing::default(),
-                        ..default()
-                    },
-                    // We can also change color of the overlay
-                    text_color: Color::srgb(128.0, 128.0, 128.0),
-                    // We can also set the refresh interval for the FPS counter
-                    refresh_interval: core::time::Duration::from_millis(100),
-                    enabled: true,
-                    frame_time_graph_config: FrameTimeGraphConfig {
-                        enabled: true,
-                        // The minimum acceptable fps
-                        min_fps: 30.0,
-                        // The target fps
-                        target_fps: 144.0,
-                    },
-                },
-            },
-        ))
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins)
         .add_plugins(Wireframe2dPlugin::default())
         .add_plugins(ErrorPlugin)
         .add_plugins(NetworkPlugin)
@@ -70,8 +45,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_plugins(IngamePlugin)
         .insert_state(AppState::Home)
         .insert_resource(Args(Args_::parse()))
-        .add_systems(Startup, setup::setup)
-        .run();
+        .add_systems(Startup, setup::setup);
+
+    #[cfg(feature = "debug")]
+    app.add_plugins(DebugPlugin);
+
+    app.run();
 
     Ok(())
 }
