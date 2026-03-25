a62d72526f1142b0951893396864f1f0fbb9f83c
Bastien Sevajol <contact@bux.fr>
2026-03-25 21:30:06 +0100

    feat: projectile line instead point

--- crates/oc_battle_gui/src/ingame/projectile.rs
+++ crates/oc_battle_gui/src/ingame/projectile.rs
@@ -3,7 +3,9 @@ use bevy::prelude::*;
 use oc_geo::region::{Region as _, WorldRegionIndex};
 use oc_physics::Physic;
 use oc_physics::collision::{Material_, Materials};
-use oc_physics::update::bevy::{Forces, PhysicsPlugin, Position, Region, Tile, Volume};
+use oc_physics::update::bevy::{
+    Forces, PhysicsPlugin, Position, Region, SetPositionEvent, Tile, Volume,
+};
 use oc_utils::bevy::EntityMapping;
 
 use crate::entity::projectile::ProjectileId;
@@ -27,6 +29,7 @@ impl Plugin for ProjectilePlugin {
         >::default())
             .add_observer(on_insert_projectile)
             .init_resource::<EntityMapping<oc_projectile::ProjectileId>>()
+            .add_observer(on_update_position)
             .add_observer(on_forgotten_region)
             .add_observer(on_forgot_projectile)
             .add_systems(
@@ -46,6 +49,7 @@ pub fn on_insert_projectile(
 ) {
     tracing::trace!(name="spawn-projectile", i=?projectile.0, position=?projectile.1.position(), forces=?projectile.1.forces());
     let position = projectile.1.position();
+    let line = Polyline2d::new(vec![Vec2::new(position[0], position[1])]);
     let entity = commands
         .spawn((
             ProjectileId(projectile.0),
@@ -55,7 +59,7 @@ pub fn on_insert_projectile(
             Forces(projectile.1.forces().clone()),
             Material_(Materials::Traversable),
             Volume(projectile.1.volume(*position).clone()),
-            Mesh2d(meshes.add(Circle::new(2.5))),
+            Mesh2d(meshes.add(line)),
             MeshMaterial2d(materials.add(Color::from(RED))),
             Transform::from_xyz(
                 projectile.1.position()[0],
@@ -65,6 +69,29 @@ pub fn on_insert_projectile(
         ))
         .id();
     state.insert(projectile.0, entity);
+}
+
+fn on_update_position(
+    position: On<SetPositionEvent<oc_projectile::ProjectileId>>,
+    projectiles: Res<EntityMapping<oc_projectile::ProjectileId>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    query: Query<&Mesh2d>,
+) {
+    let (i, position, previous) = (position.0, &position.1, &position.2);
+    let Some(entity) = projectiles.get(&i) else {
+        return;
+    };
+    let Ok(mesh) = query.get(*entity) else {
+        return;
+    };
+    let Some(mesh) = meshes.get_mut(mesh) else {
+        return;
+    };
+
+    let position = Vec2::new(position[0], position[1]);
+    let previous = Vec2::new(previous[0], previous[1]);
+    let relative = previous - position;
+    *mesh = Polyline2d::new(vec![Vec2::new(0.0, 0.0), relative]).into();
 }
 
 // TODO: should be automatized (macro? derive ?)
