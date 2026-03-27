8673989f0150b74ffc2072279effe07503812c92
Bastien Sevajol <contact@bux.fr>
2026-03-27 22:19:06 +0100

    chore: root const from env var

--- crates/oc_root/src/lib.rs
+++ crates/oc_root/src/lib.rs
@@ -2,17 +2,17 @@ pub mod config;
 pub mod ids;
 pub mod physics;
 
-pub const WORLD_WIDTH: usize = 1_000;
-pub const WORLD_HEIGHT: usize = 1_000;
-pub const REGION_WIDTH: usize = 100;
-pub const REGION_HEIGHT: usize = 100;
+pub const WORLD_WIDTH: usize = _usize(option_env!("WORLD_WIDTH"), "1000");
+pub const WORLD_HEIGHT: usize = _usize(option_env!("WORLD_WIDTH"), "1000");
+pub const REGION_WIDTH: usize = _usize(option_env!("WORLD_WIDTH"), "100");
+pub const REGION_HEIGHT: usize = _usize(option_env!("WORLD_WIDTH"), "100");
 
 pub const TILES_COUNT: usize = WORLD_WIDTH * WORLD_HEIGHT;
 pub const REGIONS_COUNT: usize = TILES_COUNT / (REGION_WIDTH * REGION_HEIGHT);
 pub const REGIONS_WIDTH: usize = WORLD_WIDTH / REGION_WIDTH;
 pub const REGIONS_HEIGHT: usize = WORLD_HEIGHT / REGION_HEIGHT;
 
-pub const INDIVIDUALS_COUNT: usize = 10;
+pub const INDIVIDUALS_COUNT: usize = _usize(option_env!("WORLD_WIDTH"), "10");
 pub const INDIVIDUAL_TICK_INTERVAL_US: u64 = 1_000_000 / 4;
 
 pub const PHYSICS_TICK_PER_SECONDS: u64 = 10;
@@ -29,8 +29,8 @@ pub const WORLD_HEIGHT_PIXELS: u64 = WORLD_HEIGHT as u64 * GEO_PIXELS_PER_TILE;
 pub const REGION_WIDTH_PIXELS: u64 = REGION_WIDTH as u64 * GEO_PIXELS_PER_TILE;
 pub const REGION_HEIGHT_PIXELS: u64 = REGION_HEIGHT as u64 * GEO_PIXELS_PER_TILE;
 
-pub const MINIMAP_WIDTH_PIXELS: usize = 2048;
-pub const MINIMAP_HEIGHT_PIXELS: usize = 2048;
+pub const MINIMAP_WIDTH_PIXELS: usize = _usize(option_env!("WORLD_WIDTH"), "2048");
+pub const MINIMAP_HEIGHT_PIXELS: usize = _usize(option_env!("WORLD_WIDTH"), "2048");
 
 const _: () = assert!(
     WORLD_WIDTH % REGION_WIDTH == 0,
@@ -40,3 +40,18 @@ const _: () = assert!(
     WORLD_HEIGHT % REGION_HEIGHT == 0,
     "World height mut be divisible by region height"
 );
+
+const fn _usize(s: Option<&str>, s2: &str) -> usize {
+    let s = match s {
+        Some(s) => s,
+        None => s2,
+    };
+    let bytes = s.as_bytes();
+    let mut result = 0usize;
+    let mut i = 0;
+    while i < bytes.len() {
+        result = result * 10 + (bytes[i] - b'0') as usize;
+        i += 1;
+    }
+    result
+}
