2b02013621b7f4d3b0bde30bb47957844a5f4d79
Bastien Sevajol <contact@bux.fr>
2026-04-23 17:21:38 +0200

    chore: wip

--- crates/oc_root/src/y.rs
+++ crates/oc_root/src/y.rs
@@ -11,41 +11,49 @@ pub trait Y<T> {
 
 impl Y<f32> for f32 {
     fn to_world_y(&self) -> f32 {
-        WORLD_HEIGHT_PIXELS as f32 + self
+        WORLD_HEIGHT_PIXELS as f32 - self
     }
 
     fn to_gui_y(&self) -> f32 {
-        WORLD_HEIGHT_PIXELS as f32 - self
+        self + WORLD_HEIGHT_PIXELS as f32 - self
     }
 }
 
 impl Y<Vec2> for Vec2 {
     fn to_world_y(&self) -> Vec2 {
-        Vec2::new(self.x, WORLD_HEIGHT_PIXELS as f32 + self.y)
+        Vec2::new(self.x, WORLD_HEIGHT_PIXELS as f32 - self.y)
     }
 
     fn to_gui_y(&self) -> Vec2 {
-        Vec2::new(self.x, WORLD_HEIGHT_PIXELS as f32 - self.y)
+        Vec2::new(self.x, self.y + WORLD_HEIGHT_PIXELS as f32 - self.y)
     }
 }
 
 #[cfg(feature = "bevy")]
 impl Y<bevy::math::Vec2> for bevy::math::Vec2 {
     fn to_world_y(&self) -> bevy::math::Vec2 {
-        bevy::math::Vec2::new(self.x, WORLD_HEIGHT_PIXELS as f32 + self.y)
+        bevy::math::Vec2::new(self.x, self.y + WORLD_HEIGHT_PIXELS as f32 - self.y)
     }
 
     fn to_gui_y(&self) -> bevy::math::Vec2 {
-        bevy::math::Vec2::new(self.x, WORLD_HEIGHT_PIXELS as f32 - self.y)
+        bevy::math::Vec2::new(self.x, self.y + WORLD_HEIGHT_PIXELS as f32 - self.y)
     }
 }
 
 impl Y<[f32; 3]> for [f32; 3] {
     fn to_world_y(&self) -> [f32; 3] {
-        [self[0], WORLD_HEIGHT_PIXELS as f32 + self[1], self[2]]
+        [
+            self[0],
+            -self[1] + WORLD_HEIGHT_PIXELS as f32 - self[1],
+            self[2],
+        ]
     }
 
     fn to_gui_y(&self) -> [f32; 3] {
-        [self[0], WORLD_HEIGHT_PIXELS as f32 - self[1], self[2]]
+        [
+            self[0],
+            -self[1] + WORLD_HEIGHT_PIXELS as f32 - self[1],
+            self[2],
+        ]
     }
 }
